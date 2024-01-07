ed25dea2c04767eddae5098efadbffd068c56828
Piotr Kowalski <pkowalski7@sii.pl>
2024-01-07 21:26:06 +0100

    na branchu merge

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import com.example.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
+
+        val btnPrzycisk = findViewById<Button>(R.id.mybutton)
+        btnPrzycisk.text = "haha"
     }
 }
